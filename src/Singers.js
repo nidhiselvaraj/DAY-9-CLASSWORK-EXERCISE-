18f3e0a8713623195af91b90693c187a514f5a31
nidhiselvaraj <108916818+nidhiselvaraj@users.noreply.github.com>
2023-04-04 20:22:01 +0530

    Add files via upload

---
+++ src/Singers.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Singers() {
+  return (
+    <div>
+        <h4>Sridhar Sena</h4>
+        <h4>2020</h4>
+        <h4>Anirudh</h4>
+        <h4>2003</h4>
+        <h4>AR Rahman</h4>
+        <h4>1920</h4>
+    </div>
+  )
+}
+
+export default Singers
